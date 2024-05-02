bb812f7c91a3d2587b7da0f6aa7a284a8562b25b
Benjamin Jørgensen <me@benmi.me>
2024-05-02 21:03:35 +0900

    uncomment the kick starter plugins

--- init.lua
+++ init.lua
@@ -565,15 +565,15 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-         clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {
           imports = {
             granularity = {
-              group = "module",
+              group = 'module',
             },
-            prefix = "self",
+            prefix = 'self',
           },
           cargo = {
             buildScripts = {
@@ -581,8 +581,8 @@ require('lazy').setup({
             },
           },
           procMacro = {
-            enable = true
-          }
+            enable = true,
+          },
         },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -889,11 +889,11 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
